7ddfdfff885f08239c23fb5302c72d9fd7846bc2
Wowo10 <wojtekwojtek529@gmail.com>
2020-09-20 21:49:09 +0200

    Implemented Repo and used.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 mod redis_repo;
+use std::env;
 
 fn main() {
 
+    let args: Vec<String> = env::args().collect();
+    let d = args.last().unwrap();
+
+    println!("{:?}",args);
+
     let repo = redis_repo::RedisRepo::create("redis://127.0.0.1/");
 
-    repo.save("dupa2", "15").unwrap();
-    let value = repo.read("dupa2").unwrap();
-    println!("{}", value);
+    let list = repo.list_all();
+    
+    for pair in list {
+        println!("{} : {}", pair.0, pair.1)    
+    }
+
+    // repo.save("dupa2", "15").unwrap();
+    // let value = repo.read("dupa2").unwrap();
+    // println!("{}", value);
 }
---
+++ src/redis_repo/mod.rs
@@ -0,0 +1,50 @@
+extern crate redis;
+
+use std::collections::HashMap;
+
+pub struct RedisRepo{
+    client: redis::Client,
+}
+
+impl RedisRepo{
+    pub fn create(address: &str) -> Self{
+        RedisRepo{
+            client: redis::Client::open(address).unwrap()
+        }
+    }
+
+    pub fn save(&self, key: &str, value: &str) -> redis::RedisResult<()> {
+        let mut connection = self.client.get_connection().unwrap();
+
+        let _ : () = redis::cmd("SET").arg(key).arg(value).query(&mut connection)?;
+        Ok(())
+    }
+    
+    pub fn read(&self, key: &str) -> redis::RedisResult<String> {
+        let mut connection = self.client.get_connection().unwrap();
+
+        let value: String = redis::cmd("GET").arg(key).query(&mut connection)?;
+        Ok(value)
+    }
+
+    fn list(&self) -> redis::RedisResult<Vec<String>>{
+        let mut connection = self.client.get_connection().unwrap();
+
+        let value: Vec<String> = redis::cmd("KEYS").arg("*").query(&mut connection)?;
+        Ok(value)
+    }
+
+    pub fn list_all(&self) -> HashMap<String, String>{
+        let keys = self.list().unwrap();
+
+        let mut map = HashMap::new();
+
+        for key in keys{
+            let value = self.read(&key).unwrap();
+
+            map.insert(key, value);
+        }
+
+        map
+    }
+}

25cb0895517f757d6903f84abddb42de341bfea5
Wowo10 <wojtekwojtek529@gmail.com>
2020-09-20 20:41:43 +0200

    Separated save function.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ fn main() {
     let client = redis::Client::open("redis://127.0.0.1/").unwrap();
     let mut con = client.get_connection().unwrap();
 
-    let _ : () = redis::cmd("SET").arg("dupa").arg(42).query(&mut con).unwrap();
+    save(&mut con, "dupa", "43").unwrap();
     let dupa : i32 = redis::cmd("GET").arg("dupa").query(&mut con).unwrap();
 
     println!("{}", dupa);
+}
+
+fn save(con: &mut redis::Connection, key: &str, value: &str) -> redis::RedisResult<()> {
+    let _ : () = redis::cmd("SET").arg(key).arg(value).query(con)?;
+    Ok(())
 }

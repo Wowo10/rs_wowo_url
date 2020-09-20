f9beacdade292189e5c126f83b5a21f1c4711578
Wowo10 <wojtekwojtek529@gmail.com>
2020-09-20 21:22:09 +0200

    Got rid of obstacle code.

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,7 @@ fn main() {
 
     let repo = redis_repo::RedisRepo::create("redis://127.0.0.1/");
 
-    repo.save("dupa2", "10").unwrap();
-    repo.save("dupa3", "11").unwrap();
+    repo.save("dupa2", "15").unwrap();
     let value = repo.read("dupa2").unwrap();
     println!("{}", value);
-    let value = repo.read("dupa3").unwrap();
-    println!("{}", value);
-
-    ///////////////////////////////////////////////////
-    println!("///////////////////");
-
-    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
-    let mut con = client.get_connection().unwrap();
-
-    save(&mut con, "dupa", "44").unwrap();
-    let dupa_value = read(&mut con, "dupa").unwrap();
-
-    println!("{}", dupa_value);
-}
-
-fn save(con: &mut redis::Connection, key: &str, value: &str) -> redis::RedisResult<()> {
-    let _ : () = redis::cmd("SET").arg(key).arg(value).query(con)?;
-    Ok(())
-}
-
-fn read(con: &mut redis::Connection, key: &str) -> redis::RedisResult<String> {
-    let value: String = redis::cmd("GET").arg(key).query(con)?;
-    Ok(value)
 }

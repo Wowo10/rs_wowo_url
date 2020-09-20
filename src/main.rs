1e3e54ba7bd7725ff02f3725a97c721914e7988a
Wowo10 <wojtekwojtek529@gmail.com>
2020-09-20 20:45:31 +0200

    Separated read function.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ fn main() {
     let client = redis::Client::open("redis://127.0.0.1/").unwrap();
     let mut con = client.get_connection().unwrap();
 
-    save(&mut con, "dupa", "43").unwrap();
-    let dupa : i32 = redis::cmd("GET").arg("dupa").query(&mut con).unwrap();
+    save(&mut con, "dupa", "44").unwrap();
+    let dupa_value = read(&mut con, "dupa").unwrap();
 
-    println!("{}", dupa);
+    println!("{}", dupa_value);
 }
 
 fn save(con: &mut redis::Connection, key: &str, value: &str) -> redis::RedisResult<()> {
     let _ : () = redis::cmd("SET").arg(key).arg(value).query(con)?;
     Ok(())
+}
+
+fn read(con: &mut redis::Connection, key: &str) -> redis::RedisResult<String> {
+    let value: String = redis::cmd("GET").arg(key).query(con)?;
+    Ok(value)
 }

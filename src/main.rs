03736bd18333b0cac9f34b78ad06b0722aa1e55a
Wowo10 <wojtekwojtek529@gmail.com>
2020-09-20 22:37:45 +0200

    Implemented open delete and clear.

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,17 @@ mod redis_repo;
 use std::collections::VecDeque;
 use std::env;
 
+extern crate webbrowser;
 extern crate nanoid;
 use nanoid::nanoid;
 
 fn main() {
     let mut args: VecDeque<String> = env::args().skip(1).collect(); //skipping path arg
 
-    println!("{:?}", args);
-
     let repo = redis_repo::RedisRepo::create("redis://127.0.0.1/");
 
     match &args.pop_front().unwrap()[..] {
         "s" | "save" => {
-
             let guid = nanoid!();
             let url = args.pop_front().unwrap();
 
@@ -23,8 +21,30 @@ fn main() {
             println!("Saved as {}", guid);
             println!("Try -- open <hash>");
         },
+        "d" | "delete" | "r" | "remove" => {
+            
+            let id = args.pop_front().unwrap();
+            repo.delete(&id).unwrap();
+
+            println!("Deleted {}.", &id);
+        },
+        "o" | "open" => {
+            let id = args.pop_front().unwrap();
+
+            let url = repo.read(&id).unwrap();
+
+            println!("Opening {}.", &url);
+
+            webbrowser::open(&url).unwrap();
+        }
+        "clear" => {
+            repo.clear().unwrap();
+        }
         "list" | _ => {
             let list = repo.list_all();
+
+            println!("Listing all ulrs:");
+            println!("<Hash>: <Url>");
 
             &list
                 .iter()
--- src/redis_repo/mod.rs
+++ src/redis_repo/mod.rs
@@ -2,44 +2,61 @@ extern crate redis;
 
 use std::collections::HashMap;
 
-pub struct RedisRepo{
+pub struct RedisRepo {
     client: redis::Client,
 }
 
-impl RedisRepo{
-    pub fn create(address: &str) -> Self{
-        RedisRepo{
-            client: redis::Client::open(address).unwrap()
+impl RedisRepo {
+    pub fn create(address: &str) -> Self {
+        RedisRepo {
+            client: redis::Client::open(address).unwrap(),
         }
     }
 
     pub fn save(&self, key: &str, value: &str) -> redis::RedisResult<()> {
         let mut connection = self.client.get_connection().unwrap();
 
-        let _ : () = redis::cmd("SET").arg(key).arg(value).query(&mut connection)?;
+        let _: () = redis::cmd("SET")
+            .arg(key)
+            .arg(value)
+            .query(&mut connection)?;
         Ok(())
     }
-    
+
     pub fn read(&self, key: &str) -> redis::RedisResult<String> {
         let mut connection = self.client.get_connection().unwrap();
 
         let value: String = redis::cmd("GET").arg(key).query(&mut connection)?;
         Ok(value)
     }
+    
+    pub fn delete(&self, key: &str) -> redis::RedisResult<String> {
+        let mut connection = self.client.get_connection().unwrap();
 
-    fn list_keys(&self) -> redis::RedisResult<Vec<String>>{
+        let value: String = redis::cmd("DEL").arg(key).query(&mut connection)?;
+        Ok(value)
+    }
+    
+    pub fn clear(&self) -> redis::RedisResult<()>{
+        let mut connection = self.client.get_connection().unwrap();
+
+        let _: () = redis::cmd("FLUSHALL").query(&mut connection)?;
+        Ok(())
+    }
+
+    fn list_keys(&self) -> redis::RedisResult<Vec<String>> {
         let mut connection = self.client.get_connection().unwrap();
 
         let value: Vec<String> = redis::cmd("KEYS").arg("*").query(&mut connection)?;
         Ok(value)
     }
 
-    pub fn list_all(&self) -> HashMap<String, String>{
+    pub fn list_all(&self) -> HashMap<String, String> {
         let keys = self.list_keys().unwrap();
 
         let mut map = HashMap::new();
 
-        for key in keys{
+        for key in keys {
             let value = self.read(&key).unwrap();
 
             map.insert(key, value);
